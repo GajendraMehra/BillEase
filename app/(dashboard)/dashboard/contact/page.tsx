6d38bd2ff101a1e1758b2c5ddef04e431434eba8
gajendramehra <gajendramehra14rgnv@ngmail.com>
2024-04-11 15:02:59 +0530

    fix: gst regex

--- app/(dashboard)/dashboard/contact/page.tsx
+++ app/(dashboard)/dashboard/contact/page.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import type {Metadata} from "next";
 import {PageHeader} from "~/components/dashboard/layouts/page-header";
 import {PageContent} from "~/components/dashboard/layouts/page-content";
-import {ContactForm} from "~/components/dashboard/forms/contact-form";
 import {ContactList} from "~/components/dashboard/lists/contact-list";
-import {Separator} from "~/components/ui/separator";
 import pkg from "~/package.json";
 
 export const metadata: Metadata = {
